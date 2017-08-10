4203afaafe9043d57b41311bf804220b4d372272
Benjamin Draves <benjamin.draves@gmail.com>
2017-08-10 11:08:14 -0500

    update-cv-data

--- treelet_prepare_cv.R
+++ treelet_prepare_cv.R
@@ -1,4 +1,4 @@
-treelet_prep_cv = funtion(grm_name, no_cores = NA, num_test = 50){
+treelet_prepare_cv = function(grm_name, num_test = 50, snp_set_size = NA){
 	
 	#--------------------------------
 	#Check for errors in the grm file
@@ -8,25 +8,32 @@ treelet_prep_cv = funtion(grm_name, no_cores = NA, num_test = 50){
 		stop("please specify grm file name \n this should be in the current directory")
 	}	
 	
-	#--------------------------------
-	# Set number of cores to use
-	#--------------------------------
-	
-	if(is.na(no_cores)){
-		message("number of cores not specified \n setting to no_cores - 1")
-		no_cores = detectCores() - 1
-	}	
-	
 	#-------------------------------------------
 	# read in SNP file 
 	#-------------------------------------------
 	
-	snp_name = paste(grm_name, ".bim.gz", sep = "")
+	snp_name = paste(grm_name, ".bim", sep = "")
 	snps = as.matrix(read.table(snp_name))[,2]
 	
 	if(!exists("snps")){
 		stop("couldn't find the file")
 	}
+	
+	
+	if(is.na(snp_set_size)){
+		
+		#include entire snp set 
+		snp_set_size = length(snps)
+	}
+	
+	
+	if(length(snps)<snp_set_size){
+		message("snp_set_size is greater than the number of snps \n setting it to the max value")
+		snp_set_size = length(snps)
+	}
+	
+	snps = sample(snps, snp_set_size)
+	
 	
 	
 	#--------------------------------
@@ -60,12 +67,15 @@ treelet_prep_cv = funtion(grm_name, no_cores = NA, num_test = 50){
 		
 		#write out small_snp_set
 		file = paste("test",i, sep = "")
-		write.table(small_snp_set, file, col.names = F, row.names = F, quote = F)
+		write.table(snps[small_snp_set], file, col.names = F, row.names = F, quote = F)
 	}
 	
 	
 	#go back a directory
 	setwd("..")
+	
+	
+	message("writting out grms - this may take a very long time...")
 	
 	#--------------------------------
 	#Make training grms
@@ -73,25 +83,27 @@ treelet_prep_cv = funtion(grm_name, no_cores = NA, num_test = 50){
 	
 	#create directory for the new grms
 	dir.create("./cv_grms", showWarnings = FALSE)
+		
+	#format snp file names
+	snp_file = paste(getwd(), "/snp_sets/train", sep = "")
+	out = paste(getwd(), "/cv_grms/train", sep = "")	
 
 	#make the call to gcta 
-
-	result = foreach(i=1:num_test)%dopar%{
-		
-		#format snp file names3
-		snp_file = paste(getwd(), "/snp_sets/test",i, sep = "")
-		out = paste(getwd(), "/cv_grms/train", sep = "")	
-	}
-
-	
-	
-	
-	system(paste("./gcta_mac --bfile ",grm_name," --extract ",snp_file," --make-grm --out "sep = ""))
+	system(paste("./gcta_mac --bfile ",grm_name," --extract ",snp_file," --make-grm --out ", out, sep = ""))
 	
 	
 	#--------------------------------
 	#Make testing grms
 	#--------------------------------
+	for(i in 1:num_test){
+		
+		#format snp file names
+		snp_file = paste(getwd(), "/snp_sets/test",i, sep = "")
+		out = paste(getwd(), "/cv_grms/test",i, sep = "")	
+		
+		#make the call to gcta
+		system(paste("./gcta_mac --bfile ",grm_name," --extract ",snp_file," --make-grm --out ",out,sep = ""))
+	}
 
 	
 	
